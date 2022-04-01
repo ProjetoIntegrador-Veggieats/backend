6e476b40a1bcbf449f4dca3f650cf15cf7447830
Jenny Gritti <100284575+jennygritti@users.noreply.github.com>
2022-04-01 11:41:42 -0300

    CrudProduto

--- backend/Veggieats/src/main/java/com/generation/veggieats/controller/ProdutoController.java
+++ backend/Veggieats/src/main/java/com/generation/veggieats/controller/ProdutoController.java
@@ -3,6 +3,8 @@ package com.generation.veggieats.controller;
 import java.math.BigDecimal;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -54,7 +56,7 @@ public class ProdutoController {
     }
 	
 	@PostMapping
-	public ResponseEntity<Produto> post (@RequestBody Produto produto){
+	public ResponseEntity<Produto> post (@Valid @RequestBody Produto produto){
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produto));
 	}
 	
